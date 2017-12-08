db4784240d75fc75d79877b1debc81d63cd50029
nicolas <nicolasdimarco@gmail.com>
2017-12-08 18:47:45 -0300

    add models to surgery app with admin included

--- surgery/admin.py
+++ surgery/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Surgeon, Patient, Surgery
 
-# Register your models here.
+admin.site.register(Surgeon)
+admin.site.register(Patient)
+admin.site.register(Surgery)
--- surgery/models.py
+++ surgery/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birthdate = models.DateField('Fecha de Nacimiento')
+    dni = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "{0} {1}".format(self.first_name, self.last_name)
+
+
+class Surgeon(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "{0} {1}".format(self.first_name, self.last_name)
+
+
+class Surgery(models.Model):
+    patient = models.ForeignKey(Patient)
+    surgeon = models.ForeignKey(Surgeon)
+    date = models.DateTimeField('Fecha de Cirugía')
+
+    def __str__(self):
+        return "{0} {1}".format(self.patient.last_name, self.date)
